2e6882c8811111fbee109cf87f982bf1715c00fe
Nikos Kanellopoulos <kanellopoulos.nikos@gmail.com>
2025-03-02 15:08:33 +0200

    Moved UI celements into their own package

---
+++ ui_elements/__init__.py
@@ -0,0 +1,11 @@
+from .connection_dialog import ConnectionDialog
+from .diagram_view import ERDiagramView
+from .status_bar_manager import StatusBarManager
+from .toolbar_manager import ToolbarManager
+
+__all__ = [
+    "ConnectionDialog",
+    "ERDiagramView",
+    "StatusBarManager",
+    "ToolbarManager",
+]
